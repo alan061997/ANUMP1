5ee86a3cfcc19ebde65f2a8d04c96d7ead7a8e30
Baruc <baruc.almaguer@gmail.com>
2017-08-31 11:11:06 -0500

    Añadidos prints de debugging a calc_iter

--- algoritmo_gs.py
+++ algoritmo_gs.py
@@ -1,3 +1,6 @@
+#para funcion time.sleep
+import time
+
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
 #Matriz dominante
 def fila_dominante(r):
@@ -69,50 +72,54 @@ def datos_dominante(d):
 	return d
 		
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+def error_aproximado(act, ant):
+	Ea = abs((act-ant)/act * 100)
+	return Ea
+
+#imprime error aproximado
+def printable_Ea(var, act, ant):
+	Ea = abs((act-ant)/act * 100)
+	return "Ea({}) = {}%".format(var, Ea)
+
+def print_xact(iter, xact):
+	print("i={}, xs={}".format(iter, xact))
 
 #funcion que toma una lista y un elemento como argumento y lo "despeja" del resto:
 
 def despejar(lista, idx):
-    l_out = []
-    for (i, j) in enumerate(lista):
-        if j != lista[idx]:
-            l_out.append(j if (i == len(lista) - 1) else -j)
-        else:
-            l_out.append(0) #el despejado, se agrega para no perder la referencia por indice de las demas
-    return l_out
+	l_out = []
+	for (i, j) in enumerate(lista):
+		if j != lista[idx]:
+			l_out.append(j if (i == len(lista) - 1) else -j)
+		else:
+			l_out.append(0) #el despejado, se agrega para no perder la referencia por indice de las demas
+	return l_out
 
 #funcion que calcula una iteracion de Xn para dadas Xn anteriores y la matriz a resolver
 def calc_iter(matrix, x_ant):
-    x_act = list(x_ant)
-    for i, row in enumerate(matrix):
-        despejado = despejar(row, i)
-        num_list = [d * x for d, x in zip(x_act, despejado[0:-1])]
-        num = despejado[-1] + sum(num_list)
-        den = row[i]
-        x_act[i] = num/den
-    return x_act
-
-def error_aproximado(act, ant):
-    Ea = abs((act-ant)/act * 100)
-    return Ea
-
-#imprime error aproximado
-def printable_Ea(var, act, ant):
-    Ea = abs((act-ant)/act * 100)
-    return "Ea({}) = {}%".format(var, Ea)
+	x_act = list(x_ant)
+	for i, row in enumerate(matrix):
+		despejado = despejar(row, i)
+		num_list = [d * x for d, x in zip(x_act, despejado[0:-1])]
+		num = despejado[-1] + sum(num_list)
+		den = row[i]
+		x_act[i] = num/den
+	return x_act
 
 def gauss_seidel(matrix, max_error):
-    x_ant = [0.1 for i in range(len(matrix))]
-    x_act = [0.1 for i in range(len(matrix))]
-    error = 100.0
-    iters = 0
-    while error >= max_error:
-        x_act = calc_iter(matrix, x_ant)
-        errors = [error_aproximado(xac, xan) for xac, xan in zip(x_act, x_ant)]
-        error = max(errors)
-        x_ant = x_act
-        iters+=1
-    return {"valores": x_act, "ea": error, "iters": iters}
+	x_ant = [0.1 for i in range(len(matrix))]
+	x_act = [0.1 for i in range(len(matrix))]
+	error = 100.0
+	iters = 0
+	while error >= max_error:
+		x_act = calc_iter(matrix, x_ant)
+		print_xact(iters, x_act)
+		time.sleep(1.0)
+		errors = [error_aproximado(xac, xan) for xac, xan in zip(x_act, x_ant)]
+		error = max(errors)
+		x_ant = x_act
+		iters+=1
+	return {"valores": x_act, "ea": error, "iters": iters}
 
 """
 #Prueba de escritorio:
